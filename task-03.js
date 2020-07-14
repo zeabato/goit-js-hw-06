661fd0e92000cf68c437b1bc7cf56a55b1072ab6
Artem <39087318+zeabato@users.noreply.github.com>
2020-07-14 11:55:30 +0300

    chenge 3,4 task

--- task-03.js
+++ task-03.js
@@ -3,7 +3,7 @@
 import users from './users.js';
 
 const getUsersWithGender = (users, gender) => {
-  return users.filter(elem=>elem.gender===gender);
+  return users.filter(elem=>elem.gender===gender).map(obj=>obj.name);
 };
 
 console.log(getUsersWithGender(users, 'male')); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
--- task-04.js
+++ task-04.js
@@ -3,6 +3,6 @@
 import users from './users.js';
 
 const getInactiveUsers = users => 
-  users.filter(elem=>elem.isActive===true);
+  users.filter(elem=>elem.isActive!==true);
 
 console.log(getInactiveUsers(users)); // [объект Moore Hensley, объект Ross Vazquez, объект Blackburn Dotson]
